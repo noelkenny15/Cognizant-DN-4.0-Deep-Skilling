feec41b4373c0eef79e0db05da9a9978d9f91c43
noelkenny15 <ch.noelkenny@gmail.com>
2025-08-14 21:10:09 +0530

    Add files via upload

---
+++ Week5/cognizant_week5_handson_modules/microservices_submission/microservices/account-service/src/main/java/com/cognizant/account/AccountController.java
@@ -0,0 +1,16 @@
+package com.cognizant.account;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+public class AccountController {
+    @GetMapping("/accounts/{number}")
+    public Map<String, Object> getAccount(@PathVariable String number) {
+        return Map.of(
+            "number", number,
+            "type", "savings",
+            "balance", 234343
+        );
+    }
+}
